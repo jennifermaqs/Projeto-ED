3b5cdbb29f7df1a4b5d3ff17cedd73a0e0dc3327
jao <joao.lira@alu.ufc.br>
2025-01-30 22:53:05 -0300

    funcoes corrigidas aeeeeeeeee

---
+++ SparseLib/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "SparseMatrix.h"
+using namespace std;
+
+int main() {
+    
+    //MAIN DE TESTE APENAS 
+    
+    int n=3;
+    int v;
+    
+    cout<<"criei a matriz 3x3";
+    
+    SparseMatrix m (3, 3);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout<<"valor (" << i <<", "<< j <<"): ";
+            cin>>v;
+            m.insert(i, j, v);
+        }
+    }
+    
+    cout<<endl;
+    cout<<"Print da matriz:"<<endl;
+    m.print();
+    
+    cout<<endl;
+    
+    cout<<"GET(1, 2):";
+    cout<<m.get(1, 2);
+
+    return 0;
+}
