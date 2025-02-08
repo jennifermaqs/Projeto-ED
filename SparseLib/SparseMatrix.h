5e693006395a9ddafe6cb1a24ffa08030dc5e3dc
jao <joao.lira@alu.ufc.br>
2025-02-08 17:04:37 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- SparseLib/SparseMatrix.h
+++ SparseLib/SparseMatrix.h
@@ -3,7 +3,7 @@
 #include "Node.h"
 #include <stdexcept>
 #include <iostream>
-
+using namespace std;
 
 class SparseMatrix {
 private:
@@ -13,31 +13,215 @@ private:
 	int colunas;
 	int linhas;
 
-	//funcao aux para limpar memória
-	void clear();
+	//funcao aux para limpar memC3ria
+	//void clear();
 
 public:
+	SparseMatrix(){
+        sentinela = new Node(0, 0, 0, nullptr, nullptr);
+		sentinela->direita = sentinela;
+		sentinela->abaixo = sentinela;
+    }
+    
 	// construtor
-	SparseMatrix(int n, int m);
+	SparseMatrix(int n, int m) {
 
-	//destrutor
-	~SparseMatrix();
+		// verificaC'C#o
+		if(n<=0 || m<=0) {
+			throw std::invalid_argument("nC:mero de linha ou coluna invC!lidos");
+			
+		}
 
-	void insert(int i, int j, double valor);
-
-	double get(int i, int j) const;
+		//inicia o sentinela principal e deixa circular
+		sentinela = new Node(0, 0, 0, nullptr, nullptr);
+		sentinela->direita = sentinela;
+		sentinela->abaixo = sentinela;
+		//AQUI JA TA CIRCULAR
+		//atualiza o nC:mero de linhas e de colunas
+		linhas = n;
+		colunas = m;
+		
+		auxConstrutor(sentinela, n, m);
+		//AQUI JA TA CIRCULAR
+	}
 	
-	void print() const;
+	void auxConstrutor(Node* sentinela, int n, int m){
+	    
+		//atualiza o nC:mero de linhas e de colunas
+		linhas = n;
+		colunas = m;
+	    
+	    //sentinelas das linhas
+		this->linSentinela = new Node(1, 0, 0, nullptr, nullptr);
+		linSentinela->direita = linSentinela; //deixa circular horizontalmente
+		sentinela->abaixo = linSentinela;
+		for(int i=2; i<=n; i++) {
+			linSentinela->abaixo = new Node(i, 0, 0, nullptr, nullptr);
+			linSentinela = linSentinela->abaixo;
+			linSentinela->direita = linSentinela;
+		}
+		linSentinela->abaixo = sentinela; //deixei circular
 
-	friend SparseMatrix sum(const SparseMatrix& A, const SparseMatrix& B);
-	friend SparseMatrix multiply(const SparseMatrix& A, const SparseMatrix& B);
+		//sentinelas das colunas
+		this->colSentinela = new Node(0, 1, 0, nullptr, nullptr);
+		colSentinela->abaixo = colSentinela;
+		sentinela->direita = colSentinela;
+		for(int i=2; i<=m; i++) {
+			colSentinela->direita = new Node(0, i, 0, nullptr, nullptr);
+			colSentinela = colSentinela->direita;
+			colSentinela->abaixo = colSentinela;
+		}
+		colSentinela->direita = sentinela;
+	}
+
+	void clear() {
+		//desalocando as memorias das linhas
+		Node* linAtual = sentinela->abaixo; // primeira linha
+		while (linAtual != sentinela) {
+			Node* noAtual = linAtual->direita;
+			while(noAtual != linAtual) {
+				Node* temp = noAtual;
+				noAtual = noAtual -> direita;
+				delete temp; // deletando no
+			}
+			Node* nextLin = linAtual->abaixo;
+			delete linAtual; // deletando sentinela da linha
+			linAtual = nextLin; // vai pra proxima linha
+		}
+		//desalocando as memorias das colunas
+		Node* colAtual = sentinela->direita; //primeira coluna
+		while(colAtual != sentinela) {
+			Node* nextCol = colAtual->direita;
+			delete colAtual; // deletando sentinela da coluna
+			colAtual = nextCol; // proxima coluna
+		}
+	}
+
+	int getLinhas() const {
+		return linhas;
+	}
+
+	int getColunas() const {
+		return colunas;
+	}
+
+	~SparseMatrix() {
+		clear();
+		delete sentinela; // liberando no sentinela principal
+	}
+
+	void insert(int i, int j, double valor) {
+
+		if(valor==0) {
+			return;
+		}
+
+		if(i<=0 || j<=0 || i > linhas || j > colunas) {
+			throw std::invalid_argument("numero de linha ou coluna invalidos"); //nC#o pode acentuar :(
+		}
+
+		//vou achar o sentinela da linha que a gente esta procurando
+		Node* auxlinha = sentinela->abaixo;
+
+		while(auxlinha->linha != i) {
+			auxlinha = auxlinha->abaixo;
+		}
 
 
+		Node *lAnterior = auxlinha; //esse aqui vai achar o anterior
+		Node *lCerto = lAnterior->direita;//("lCerto de linhaCerta") //esse vai navegar sempre um a frente do auxlinha
+
+		//impede que fique em loop ao comparar com o sentinela(auxlinha) e acha o anterior do procurado
+		while(lCerto != auxlinha && lCerto->coluna < j) {
+			lAnterior = lCerto;
+			lCerto = lCerto->direita;
+		}
+
+		//vamo verificar se essa posicao ja existe
+		if(auxlinha != lCerto && lCerto->coluna == j) {
+			lCerto->value = valor;
+			return;
+		} else {
+			//caso posicao nao exista
+
+			Node* noAtual = new Node(i, j, valor, nullptr, nullptr);
+
+			//inserindo na linha que a gente achou ali e em seguida vamos achar a coluna, igual
+			//fizemos com a linha
+
+			noAtual->direita = lCerto;
+			lAnterior->direita = noAtual;
+
+			Node* auxColuna = sentinela->direita;
+			while(auxColuna->coluna != j) {
+				auxColuna = auxColuna->direita;
+			}
+
+			Node* cAnterior = auxColuna;
+			Node* cCerto = cAnterior->abaixo; // "c de coluna e "certo" de certo
+
+			while(cCerto != auxColuna && cCerto->linha < i) {
+				cAnterior = cCerto;
+				cCerto = cCerto->abaixo;
+			}
+
+
+			//agora vamo ajustar os ponteiros
+			noAtual->abaixo = cCerto;
+			cAnterior->abaixo = noAtual;
+		}
+
+	}
+
+	double get(int i, int j) const {
+		if(i<=0 || j<=0 || i > linhas || j > colunas) {
+			throw std::invalid_argument("numero de linha ou coluna invalidos");
+		}
+
+		Node* aux = sentinela;
+
+		while(aux->linha != i) {
+			aux = aux->abaixo;
+		}
+
+		while(aux->coluna != j) {
+			aux = aux->direita;
+			if(aux->linha == 0) {
+				return 0; //nao sei se essa logica ta certa, mas se a linha for 0 signica que deu a volta
+			}            //e que voltou pro sentinela, logo, o valor nao tava armazenado pois era 0 e n foi armazenado
+		}
+
+		return aux->value;
+
+	}
+
+	void print() const {
+
+		if(linhas == 0 || colunas == 0) {
+			throw std::invalid_argument("numero de linha ou coluna invalidos");
+		}
+
+		Node* aux = sentinela->abaixo;
+
+		for(int i=1; i<=linhas; i++) {
+			Node* auxColuna = aux->direita;
+			for(int j=1; j<=colunas; j++) {
+
+				if(auxColuna == sentinela) break;
+
+				if(auxColuna->coluna == j) {
+					std::cout<<" " << auxColuna->value <<" ";
+					auxColuna = auxColuna->direita;
+				} else {
+					std::cout<<" 0 ";
+				}
+			}
+			std::cout<<std::endl;
+			aux = aux->abaixo;
+		}
+	}
 
 
 };
-
-SparseMatrix sum(const SparseMatrix& A, const SparseMatrix& B);
-SparseMatrix multiply(const SparseMatrix& A, const SparseMatrix& B);
 
 #endif
--- SparseLib/main.cpp
+++ SparseLib/main.cpp
@@ -1,11 +1,111 @@
 #include <iostream>
-
 #include "SparseMatrix.h"
+#include <string>
+#include <sstream>
+#include <fstream>
 using namespace std;
 
-int main() {
-   
 
-
+void help(){
+    
+    cout<<"|================================================================|"<<endl;
+    cout<<"|---------------------------- HELP ------------------------------|"<<endl;
+    cout<<"|================================================================|"<<endl;
+    cout<<"|___________________________COMANDOS:____________________________|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|1.Exit.........................................close the program|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|2.Create m n........create new matrix whith m rows and n columns|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|3.Show i......................print the matrix i in the terminal|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|4.Showidx...................show all the indexes of rhe matrices|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|5.Sum i j...............................sum the matrices i and j|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|6.Clear i.....................................clear the matrix i|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|7.Read 'mtx.txt'.........read the matrix from the file 'mtx.txt'|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|8.Update m i j......valueupdate the value of the cell(i,j) int m|"<<endl;
+    cout<<"|                                                                |"<<endl;
+    cout<<"|9.Erase all.....erase all the matrices currently int the program|"<<endl;
+    cout<<"|________________________________________________________________|"<<endl;
     
 }
+
+
+SparseMatrix* sum(const SparseMatrix* A, const SparseMatrix* B) {
+    
+	if(A->getLinhas() != B->getLinhas()  || A->getColunas()  != B->getColunas()) {
+		throw std::invalid_argument("Matrizes com dimensC5es incompatC-veis para soma");
+	}
+    
+    int n = A->getLinhas();
+    int m = B->getColunas();
+    
+    SparseMatrix* C = new SparseMatrix(n, m);
+	//SparseMatrix C(A.getLinhas(), A.getColunas()); // matriz resultado
+	
+	for(int i=1; i<=A->getLinhas(); i++){
+	    for(int j=1; j<=A->getColunas(); j++){
+	        C->insert(i, j, (A->get(i, j) + B->get(i, j)));
+	    }
+	}
+
+	return C;
+}
+
+
+
+int main() {
+    
+    cout<<"|================================================================|"<<endl;
+    cout<<"|---------------------------- Bem vindo! ------------------------|"<<endl;
+    cout<<"|================================================================|"<<endl;
+    cout<<"|Digite:                          |Para:                         |"<<endl;
+    cout<<"|_________________________________|______________________________|"<<endl;
+    cout<<"|-Help                            |Abrir painel de comandos      |"<<endl;
+    cout<<"|-Exit                            |Fechar programa               |"<<endl;
+    cout<<"|_________________________________|______________________________|"<<endl;
+    
+    cout<<endl;
+    
+    string comando;
+    cout<<"Digite a ação:"<<endl;
+    getline(cin, comando);
+    //cin.ignore();
+    cout<<endl;
+    
+    cout<<endl;
+    string comando;
+    
+    while(true){
+        //lembrar de limpar o buffer antes de chamar qualquer entrada
+        
+        cout<<"Digite a ação:"<<endl;
+        getline(cin, comando);
+        //cin.ignore();
+        cout<<endl;
+        
+        if(comando == "help" || comando == "Help"){
+            help();
+        }
+        else if(comando == "exit" || comando == "Exit"){
+            cout<<"...FECHANDO PROGRAMA..."<<endl;
+            break;
+        }
+        else if(comando == ""){
+            
+        }
+        else{
+            cout<<"*COMANDO INVALIDO!"<<endl;
+        }
+    }
+    
+
+	return 0;
+}
+
+
+
